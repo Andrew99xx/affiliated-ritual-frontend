c9abb4415ddc97f5bc98b125ecc25b2e085bacf8
Andrew99xx <indranilchakraborty4sc@gmail.com>
2025-01-02 00:09:55 +0530

    added news functionalities

--- src/components/LandingComponents/Media/Media.js
+++ src/components/LandingComponents/Media/Media.js
@@ -8,12 +8,16 @@ import Alj from "./Alj.png"
 import ani from "./ani.jpeg"
 import bbc from "./bbc.png"
 import cnn from "./cnn.jpeg"
+import dailyhunt from "./dailyhunt.png"
 import hindustanMetro from "./HindustanMetro.png"
 import htimes from "./htimes.jpeg"
 import lokmatTimes from "./lokmatTimes.png"
 import ly from "./ly.png"
+// import hindustanMetro from "./HindustanMetro.png"
+import hindustan from "./hindustan-bytes.jpg"
 import ndtv from "./ndtv.jpeg"
 import reuters from "./reuters.jpeg"
+import newsNation from "./newsNation.png"
 import someNews from "./someNews.png"
 import the_hindu from "./the_hindu.jpeg"
 import the_print from "./the_print.jpeg"
@@ -22,49 +26,49 @@ import theIndianExpress from "./theIndianExpress.jpeg"
 function Media() {
     const mediaData = [
         {
-            logo: someNews,
-            name: "News Nation"
+            logo: dailyhunt,
+            name: "Daily Nation",
+            link: "https://m.dailyhunt.in/news/india/english/republic+news+india-epaper-dhfacc36dfce9c4bb68db0e89d033c921b/affiliate+ritual+leading+edtech+platform+empowering+educated+youths+to+become+financially+independent-newsid-dhfacc36dfce9c4bb68db0e89d033c921b_5794bad0854411efbfc5cb9e96709a27?sm=Y"
         },
+        {
+            logo: hindustan,
+            name: "Hindustan Bytes",
+            link: "https://www.hindustanbytes.com/affiliate-ritual-leading-ed-tech-platform-empowering-educated-youths-to-become-financially-independent"
+        },
+        
         {
             logo: hindustanMetro,
+            name: "Hindustan Metro",
+            link: "https://hindustanmetro.com/navigating-the-new-wave-of-online-earning-with-affiliate-ritual/"
         },
         {
-            logo: ahmedabadMirror,
-        },
-        {
-            logo: reuters,
-            name: "REVOI"
-        },
-        {
-            logo: hindustanMetro,
-        },
-        {
-            logo: ly,
-            name: "Everyday Subjects"
+            logo: newsNation,
+            name: "News Nation",
+            link: "https://english.newsnationtv.com/brand-stories/brand-stories-english/supriyo-chatterjee-the-man-behind-affiliate-ritual-and-his-inspiring-journey-of-self-resilience-and-tenacity-6943939"
         },
         {
             logo: abpBengala,
-            name: "ABP Ananda"
+            name: "ABP Ananda",
+            link:"https://bengali.abplive.com/brand-wire/revolution-in-affiliate-marketing-affiliate-ritual-2-point-0-launched-1091006"
         },
+        
         {
-            logo: hindustanMetro,
+            logo: ahmedabadMirror,
+            name: "Ahemdabad Mirror",
+            link:"https://www.ahmedabadmirror.com/affiliate-ritual-20-by-supriyo-chatterjee-a-revolutionary-move-towards-online-success/81874460.html"
         },
-        {
-            logo: someNews,
-            name: "Daily Hunt"
-        },
-        {
-            logo: ly,
-            name: "123 Men Life"
-        }
+        
     ];
 
     return (
         <div className={styles.mediaContainer}>
             <p className={styles.mediaParagraph}>Let's meet with the team of our industry expert medias. We believe that you will be able to change your life with the most advanced AI integrated training module cooked by Affiliate Ritual.</p>
-            <div className={styles.mediaItems}>
+            <div className={styles.mediaItems} >
                 {mediaData.map((item, i) => (
                     <motion.div
+                    onClick={() => {
+                        window.open(item.link, "_blank"); // Open the link in a new tab
+                    }}
                         key={i}
                         className={styles.mediaItemWrapper}
                         initial={{ opacity: 0, y: 50 }}
